5b16d0fb773c69868975818227c61c80e106ef76
Brian-Enos <brianenos9199@gmail.com>
2023-06-15 18:54:15 +0300

    Pushing the files

---
+++ JavaScript/Objects/literals.js
@@ -0,0 +1,10 @@
+
+const myObject = {
+  property: "Value !",
+  otherProperty: 67,
+  "random property": function() {
+    return "This is a small refresher on objects in JavaScript" ;
+  }
+}
+
+console.log(myObject["property"])

201038a17258c0e4ca489fe55916c61b59e79ec3
Brian-Enos <brianenos9199@gmail.com>
2023-06-16 10:09:21 +0300

    used test method to test for regex

--- JavaScript/Regular-Expressions/test-method.js
+++ JavaScript/Regular-Expressions/test-method.js
@@ -4,4 +4,5 @@ let regex = /the/ ;
 
 let myString = "Hello World";
 let myRegex = /Hello/ ;
-let result = myRegex;
+let result = myRegex.test(myString); //tests for if myRegex is in myString
+console.log(result);
